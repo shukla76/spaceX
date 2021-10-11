de1318fd7b51f58118051c48418c262055867448
Piyush <piyushshukla259@gmail.com>
2021-10-11 20:50:50 +0530

    pigsfdkuegwrfotk

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React,{ useState } from 'react';
+import "./styles/App.css";
+import Filters from "./components/Filters"
+import Launches from './components/Launches';
+
+function App() {
+  const [data, setData] = useState()
+  // console.log(data);
+  return (
+    <div className="App">
+      <Filters setData={setData}/>
+      <Launches data={data} />
+      <p className="credit">Developed by - piyush</p>
+    </div>
+  );
+}
+
+export default App;
